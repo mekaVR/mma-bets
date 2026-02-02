f380675e0b4e9b35682281f51e8ff371155f5603
mekaVR <candau.herbert@gmail.com>
2026-02-02 11:19:50 +0100

    remove hardcoded secret key and creating .env and added @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '@auth/auth.module';
 import { UsersModule } from '@users/users.module';
@@ -8,7 +9,13 @@ import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/mmaBet'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
     AuthModule,
     UsersModule,
     FightersModule,
